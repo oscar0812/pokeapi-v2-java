2c50e3d6f2dc6dca66b614f9282db46c0bcd5a07
oscar0812 <oscar0812torres@gmail.com>
2018-03-28 12:07:17 -0500

    Name Model complete

---
+++ src/models/Name.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Name {
+    // The localized name for an API resource in a specific language
+    private String name;
+
+    // The language this name is in
+    private Language language;
+
+    public Name(String name, Language language){
+        this.name = name;
+        this.language = language;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+}
