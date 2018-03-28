9c4cfab06483b0f8cdb83ec1625f451c775bb339
oscar0812 <oscar0812torres@gmail.com>
2018-03-28 10:44:06 -0500

    added Description Model

---
+++ src/models/Description.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Description {
+    // The localized description for an API resource in a specific language
+    private String description = "";
+
+    // The language this name is in
+    private Language language;
+
+    public Description(String description, Language language){
+        this.description = description;
+        this.language = language;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+}
