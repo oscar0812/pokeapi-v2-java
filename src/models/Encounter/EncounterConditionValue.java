11aecc1886ffd5022ea15c2d394d312f88febe31
oscar0812 <oscar0812torres@gmail.com>
2018-03-28 11:58:04 -0500

    EncounterConditionValue Model Complete

---
+++ src/models/Encounter/EncounterConditionValue.java
@@ -0,0 +1,62 @@
+package models.Encounter;
+
+/*
+    example JSON:
+
+    {
+        "id": 1,
+        "name": "swarm-yes",
+        "condition": {
+            "name": "swarm",
+            "url": "http://pokeapi.co/api/v2/encounter-condition/1/"
+        },
+        "names": [{
+            "name": "WÃ¤hrend eines Schwarms",
+            "language": {
+                "name": "de",
+                "url": "http://pokeapi.co/api/v2/language/6/"
+            }
+        }]
+    }
+
+ */
+
+import models.Name;
+import models.resource.NamedAPIResource;
+
+import java.util.ArrayList;
+
+public class EncounterConditionValue extends NamedAPIResource {
+    // The identifier for this encounter condition value resource
+    private int id;
+
+    // The condition this encounter condition value pertains to
+    public EncounterCondition condition;
+
+    // The name of this encounter condition value listed in different languages
+    public ArrayList<Name> names;
+
+
+    public EncounterConditionValue(String url, String name, int id, EncounterCondition condition){
+        this.url = url;
+        this.name = name;
+        this.id = id;
+        this.condition = condition;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public EncounterCondition getCondition() {
+        return condition;
+    }
+
+    public ArrayList<Name> getNames() {
+        return names;
+    }
+
+    public void setNames(ArrayList<Name> names) {
+        this.names = names;
+    }
+}
