435e55e2656d890ac395aac943fcb8cf42b3c599
oscar0812 <oscar0812torres@gmail.com>
2018-03-28 12:03:38 -0500

    FlavorText Model Complete

---
+++ src/models/FlavorText.java
@@ -0,0 +1,22 @@
+package models;
+
+public class FlavorText {
+    // The localized flavor text for an API resource in a specific language
+    private String flavor_text;
+
+    // The language this name is in
+    private Language language;
+
+    public FlavorText(String flavor_text, Language language){
+        this.flavor_text = flavor_text;
+        this.language = language;
+    }
+
+    public String getFlavorText() {
+        return flavor_text;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+}
