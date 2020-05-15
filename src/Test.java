92101b48f9656c14808457cceb8f4f28251eaff5
oscar0812 <oscar0812torres@gmail.com>
2020-05-14 19:18:13 -0500

    added gitignore

--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,14 @@
-import pokeapi.bittle.models.moves.Move;
-import pokeapi.bittle.models.moves.MoveCategory;
-import pokeapi.bittle.models.pokemon.Pokemon;
+import pokeapi.bittle.models.items.Item;
 import pokeapi.bittle.models.resources.NamedAPIResourceList;
 import pokeapi.bittle.models.utility.NamedAPIResource;
 import pokeapi.bittle.utils.Client;
 
 public class Test {
     public static void main(String[] args) {
-        NamedAPIResourceList list = Client.getMoveCategoryList(10, 0);
-
-        for (NamedAPIResource nar:list.getResults()) {
-            MoveCategory mc = MoveCategory.getByName(nar.getName());
-            for(Move move: mc.getMoves()) {
-                System.out.println(move.get());
-            }
+        NamedAPIResourceList list = Client.getItemList(40, 20);
+        for(NamedAPIResource a: list.getResults()) {
+            System.out.println(Item.getByName(a.getName()));
         }
+        int a = 1;
     }
 }
